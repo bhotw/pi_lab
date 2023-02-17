eda785f330b9f6b4f25546cf6fe8fa4cbfb58588
Md Raspberry Pi <mdsheik633@hotmail.com>
2023-02-16 20:12:44 -1000

    this one will turn one the spicific color led for each button color

---
+++ buttonForColor.py
@@ -0,0 +1,59 @@
+import time
+import RPi.GPIO as GPIO
+
+# Pin deifinitions
+# buttons
+btn_blue = 4
+btn_yell = 5
+btn_genn = 6
+btn_redd = 12
+
+
+# LEDS
+led_redd = 18
+led_genn = 19
+led_yell = 20
+led_blue = 21
+
+GPIO.setwarnings(False)
+
+# Set up pins
+GPIO.setmode(GPIO.BCM)
+
+# Buttons
+GPIO.setup(btn_blue, GPIO.IN)
+GPIO.setup(btn_genn, GPIO.IN)
+GPIO.setup(btn_yell, GPIO.IN)
+GPIO.setup(btn_redd, GPIO.IN)
+
+# Leds
+GPIO.setup(led_redd, GPIO.OUT)
+GPIO.setup(led_genn, GPIO.OUT)
+GPIO.setup(led_yell, GPIO.OUT)
+GPIO.setup(led_blue, GPIO.OUT)
+try:
+	while True:
+		if(GPIO.input(btn_genn)):
+			GPIO.output(led_genn, GPIO.LOW)
+		else:				 
+			GPIO.output(led_genn, GPIO.HIGH)
+			
+		if(GPIO.input(btn_redd)):
+			GPIO.output(led_redd, GPIO.LOW)
+		else:				 
+			GPIO.output(led_redd, GPIO.HIGH)
+			
+		if(GPIO.input(btn_yell)):
+			GPIO.output(led_yell, GPIO.LOW)
+		else:				 
+			GPIO.output(led_yell, GPIO.HIGH)
+			
+		if(GPIO.input(btn_blue)):
+			GPIO.output(led_blue, GPIO.LOW)
+		else:				 
+			GPIO.output(led_blue, GPIO.HIGH)
+			
+		
+		
+finally:
+	GPIO.cleanup()
--- button_imput.py
+++ button_imput.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as gpio
 
+
 # Pins deifinitions
 btn1_pin = 4
 led_pin = 12
@@ -12,11 +13,14 @@ gpio.setmode(gpio.BCM)
 gpio.setup(btn1_pin, gpio.IN)
 gpio.setup(led_pin, gpio.OUT)
 
+
+
 # If button is pushed, light up LED
 x = 0
 try:
     while True:
         if gpio.input(btn1_pin):
+            print ("button pressed",btn1_pin)
             x = 0
         else:
             x = 1
